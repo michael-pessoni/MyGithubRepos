30eff4e5d1bd5e222e47389fa66eefa831ae8586
michael-pessoni <pessoni94@gmail.com>
2021-09-23 08:58:52 -0300

    Create ActionBar and search menu

--- app/src/main/java/com/michaelpessoni/mygithubrepos/App.kt
+++ app/src/main/java/com/michaelpessoni/mygithubrepos/App.kt
@@ -1,8 +1,6 @@
 package com.michaelpessoni.mygithubrepos
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
--- app/src/main/java/com/michaelpessoni/mygithubrepos/ui/MainActivity.kt
+++ app/src/main/java/com/michaelpessoni/mygithubrepos/ui/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.michaelpessoni.mygithubrepos.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import com.michaelpessoni.mygithubrepos.R
 import com.michaelpessoni.mygithubrepos.databinding.ActivityMainBinding
 
-class MainActivity: AppCompatActivity() {
+class MainActivity: AppCompatActivity(), SearchView.OnQueryTextListener {
+
+    companion object {
+        const val TAG = "TAG"
+    }
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
@@ -14,4 +22,22 @@ class MainActivity: AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(p0: String?): Boolean {
+        Log.e(TAG, "onQueryTextSubmit: $p0")
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        Log.e(TAG, "onQueryTextChange: $p0")
+        return false
+    }
+
 }
